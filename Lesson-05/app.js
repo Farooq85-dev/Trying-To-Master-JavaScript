1c4ac02c48a9fd5306765dbf3f02f8b5b6f3dc73
Muhammad Farooq <irfankhans786787788@gmail.com>
2024-12-29 11:41:05 +0500

    Lesson-05

---
+++ Lesson-05/app.js
@@ -0,0 +1,15 @@
+// Operators:-
+
+// Javascript operators are used to perform different types of mathematical and logical computations.
+
+// Types of Operators:-
+
+// Arthimetic Operators
+// Assignment Operators
+// Logical Operators
+// Comparison Operators
+// Type Oprators
+// String Operator
+// Bitwise Operator
+// Ternory Operator
+
