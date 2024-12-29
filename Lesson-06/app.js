4b0545e0545c92b28aaa93bb8b4c3f53b7fe86da
Muhammad Farooq <irfankhans786787788@gmail.com>
2024-12-29 11:55:21 +0500

    Lesson-06

---
+++ Lesson-06/app.js
@@ -0,0 +1,52 @@
+// Arthimetic Operator
+
+// Addition operator
+let a = 1;
+let b = 2;
+let c = a + b;
+console.log(c); // 3
+
+// Multiplication operator
+let d = 1;
+let e = 2;
+let f = e * d;
+console.log(f); // 2
+
+// Division Operator
+let g = 2;
+let h = 4;
+let i = g / h;
+console.log(i); // 2
+
+// Substraction operator
+let j = 2;
+let k = 4;
+let l = j - k;
+console.log(l); // -2
+
+// Modulus operator
+let m = 3;
+let n = 4;
+let p = n % m;
+console.log(p); // 3
+
+// Pre-Increment operator
+let q = 3;
+console.log(++q); // 4
+
+// Post-Increment operator
+let r = 3;
+console.log(r++); // 3
+
+// Pre-Decrement operator
+let s = 2;
+console.log(--s); // 1
+
+// Post-Decrement operator
+let t = 1;
+console.log(s--); // 1
+
+// Test Yourself:-
+
+let x = --s + t++ - --s;
+console.log(x); // 2
