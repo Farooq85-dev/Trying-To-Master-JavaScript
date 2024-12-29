ab3d76ca16c5f3ec30e77315bd6db2689ae9bef4
Muhammad Farooq <irfankhans786787788@gmail.com>
2024-12-29 09:39:08 +0500

    Lesson-01

---
+++ Lesson-01/app.js
@@ -0,0 +1,35 @@
+// Statements In JavaScript:-
+
+// In JavaScript, a statement is like a complete instruction that tells the computer to do something. These statements are made up of different parts: Values, Operators, Keywords, Expressions, and Comments. Each statement is executed by the browser in the order it appears, line by line.
+
+// Examples of JavaScript Statements:-
+
+// Semicolons are used to separate JavaScript statements. They mark the end of a statement.
+// You can put multiple statements on one line if you separate them with semicolons.
+
+let a, b, c;
+a = 2;
+b = 3;
+c = a + b;
+
+console.log("The value of c is " + c + ".");
+
+// a = 2; b = 3; z = a + b;
+
+// Code Blocks
+
+// A code block is a group of statements that are enclosed within curly brackets {}. This allows multiple statements to be executed together.
+
+function myFunction() {
+  console.log("Hello");
+  console.log("How are you?");
+}
+
+myFunction();
+
+// White Space
+
+// JavaScript ignores extra spaces in your code. This means you can use spaces to make your code more readable without affecting how it works.
+
+console.log(10*2);
+console.log(10  *  2);
