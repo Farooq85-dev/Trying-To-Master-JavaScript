892668e676c4361ad5493db85f3f4602d0079a8e
Muhammad Farooq <irfankhans786787788@gmail.com>
2024-12-29 09:44:09 +0500

    Lesson-02

---
+++ Lesson-02/app.js
@@ -0,0 +1,19 @@
+// Comments:-
+
+// JavaScript comments can be used to explain JavaScript code, and to make it more readable. JavaScript comments can also be used to prevent execution, when testing alternative code.
+
+// Single Line Comments
+
+// Single line comments start with //.
+
+// Multi-line Comments
+
+// Multi-line comments start with /* and end with */.
+
+/*
+if (ture) {
+  console.log("True");
+} else {
+  console.log("False");
+}
+*/
