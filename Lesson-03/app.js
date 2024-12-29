45e91bf4ccfaa3614b75f826b8b0eb52bbd5887e
Muhammad Farooq <irfankhans786787788@gmail.com>
2024-12-29 11:19:45 +0500

    Lesson-03

---
+++ Lesson-03/app.js
@@ -0,0 +1,44 @@
+// JavaScript Variables:-
+// Variables are Containers for Storing Data.
+// JavaScript Variables can be declared in 4 ways:
+
+/*
+Automatically
+Using var
+Using let
+Using const
+*/
+
+// In this first example, x, y, and z are undeclared variables.
+
+// They are automatically declared when first used:
+
+x = 5;
+y = 6;
+z = x + y;
+console.log(z);
+
+// Note:- It is considered good programming practice to always declare variables before use.
+
+// Example using var
+
+var x = 5;
+var y = 6;
+var z = x + y;
+
+// Note:-
+// The var keyword was used in all JavaScript code from 1995 to 2015.
+// The let and const keywords were added to JavaScript in 2015.
+// The var keyword should only be used in code written for older browsers.
+
+// Example using let
+
+let x = 5;
+let y = 6;
+let z = x + y;
+
+// Example using const
+
+const x = 5;
+const y = 6;
+const z = x + y;
