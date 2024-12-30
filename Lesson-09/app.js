779f51cc6634ee6f250b457db9525e8cf18362bd
Muhammad Farooq <irfankhans786787788@gmail.com>
2024-12-30 09:54:29 +0500

    Lesson-09

---
+++ Lesson-09/app.js
@@ -0,0 +1,27 @@
+// Boolean:-
+
+// Truthy And False
+
+let x = 5;
+// console.log(x === 5); // true
+// console.log(x === 5); // false
+
+if (x === 5) {
+  console.log("5");
+} else {
+  console.log("Other numbers than 5");
+}
+
+console.log(!1232323);
+
+// Falsy values
+
+// 1) False
+// 2) undefined
+// 3) null
+// 4) ""
+// 5) 0
+// 6) 0n
+// 7) -0
+// 8) NaN
+
