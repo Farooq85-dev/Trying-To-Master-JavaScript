778027439311357f2a56f1cf5d13c151e73eb282
Muhammad Farooq <irfankhans786787788@gmail.com>
2024-12-30 20:21:27 +0500

    Lesson-14

---
+++ Lesson-14/app.js
@@ -0,0 +1,17 @@
+// Ternory Operator:-
+
+// Ternory means composed of 3 parts.
+// Ternory is an alternative to if-else.
+
+let success = true;
+if (success) {
+  console.log("True!");
+} else {
+  console.log("False!");
+}
+
+// Using Ternory operator
+
+// Basic syntax
+// <condition> ? True Case : False Case
+success ? "True!" : "False";
