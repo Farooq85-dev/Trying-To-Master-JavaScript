c9e22e88e9bf13743018edf4982fe0c4d8f0b491
Muhammad Farooq <irfankhans786787788@gmail.com>
2024-12-30 11:54:24 +0500

    Lesson-13

---
+++ Lesson-13/app.js
@@ -0,0 +1,30 @@
+// Comparison Operator:-
+// Comarson operator returns true or false.
+
+// == Double Equality Operator
+// Chk only value
+
+// === Tripple Equality Operator
+// It checks both datatype and value.
+
+// != Not equal to operator
+// only chk the value
+
+// !== Not double equal to operator
+// both the value and datatype
+
+// let a = "21";
+
+// if (a != 22) {
+//   console.log("Hello World1!");
+// }
+
+// if (a !== "22") {
+//   console.log("Hello World2!");
+// }
+
+let c = 1;
+console.log(c > 1); // false
+console.log(c < 2); // true
+console.log(c >= 1); // true
+console.log(c <= 2); // true
