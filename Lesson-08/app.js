5668d19b71c157e6273baf5549bf9e33bc1e9220
Muhammad Farooq <irfankhans786787788@gmail.com>
2024-12-30 09:41:57 +0500

    Lesson-08

---
+++ Lesson-08/app.js
@@ -0,0 +1,40 @@
+// Assignment operators:-
+
+// Assignment
+let a = 23;
+console.log(a);
+
+// Addition assignment operator
+let b = 2;
+b += a;
+// Same as
+// b = b + a
+console.lo = g(b);
+
+// Minus assignment operator
+let c = 2;
+c -= b;
+// Same as
+// c = c - b
+console.log(c);
+
+// Multiplication assignment operator
+let d = 2;
+d *= c;
+// Same as
+// d = d * c
+console.log(d);
+
+// Divide assignment operator
+let e = 2;
+e /= d;
+// Same as
+// e = e / d
+console.log(e);
+
+// Modulus assignment operator
+let f = 2;
+f %= 4;
+// Same as
+// f = f % 4
+console.log(f);
