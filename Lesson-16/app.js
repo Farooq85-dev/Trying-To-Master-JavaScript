3d76e09e989bf9b358abfc81caa9450a7e9274c2
Muhammad Farooq <irfankhans786787788@gmail.com>
2025-01-01 20:27:08 +0500

    Lesson-17

---
+++ Lesson-16/app.js
@@ -0,0 +1,8 @@
+// Integers, Flaoting point and decimal
+
+const int = 101; // ineteger
+const dec = 122.2331; // decimal or floating point
+
+// Actaully unlike other programming language javascript has only number data type that includes integers, decimal or floating point.
+// So there is only number in JavaScript.
+    
