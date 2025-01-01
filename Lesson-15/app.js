80a7be130ee2a31abfaa95ec8dbfc9e11e5024ed
Muhammad Farooq <irfankhans786787788@gmail.com>
2025-01-01 20:18:27 +0500

    Lesson-15

---
+++ Lesson-15/app.js
@@ -0,0 +1,38 @@
+// Data Types:-
+
+// String
+const userName = "M. Farooq";
+console.log(typeof userName); // string
+
+// Number
+const userId = 102;
+console.log(typeof userId); // number
+
+// Boolean
+const isUserVerified = true;
+console.log(typeof isUserVerified); // boolean
+
+// Array
+const arrayOfNumbers = [1, 23, 3, 3];
+console.log(typeof arrayOfNumbers); // object
+
+// Object
+const students = {
+  userName: "Farooq",
+  fatherName: "Irshad",
+};
+
+console.log(typeof students); // object
+
+// null
+const isNull = null;
+console.log(typeof isNull); // object
+
+// undefined
+const isUndefined = undefined;
+console.log(typeof isUndefined); // undefined
+
+// Date
+const now = new Date();
+console.log(typeof now); // object
+
