b5263844f2c65a7c7b55f294e7cc87985912dc6e
Muhammad Farooq <irfankhans786787788@gmail.com>
2025-01-03 20:33:21 +0500

    Lesson-22

---
+++ Lesson-22/app.js
@@ -0,0 +1,21 @@
+// String Data Type:-
+// String represents textual data.
+
+// In JavaScript we can define string via three methods.
+
+const strWithSingleQuotes = 'Hello World!';
+console.log(typeof strWithSingleQuotes); // string
+
+const strWithDoubleQuotes = "Hello World!";
+console.log(typeof strWithDoubleQuotes); // string
+
+const strWithBackitcs = `Hello World!`;
+console.log(typeof strWithBackitcs); // string
+
+// Escaping character with double quotes
+const nestedDoubleQuotes = "My name is Muhammad Farooq. I doesn\"t like carrots.";
+console.log(nestedDoubleQuotes);
+
+// Escaping character with single quotes
+const nestedSingleQuotes = 'Hello Man\'s!'
+console.log(nestedSingleQuotes);
